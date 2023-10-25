d66a176d0cdc1a9a61ccac7907c3e3509043b223
JenyaKirakosyan27 <147413285+JenyaKirakosyan27@users.noreply.github.com>
2023-10-25 23:13:38 +0400

    Update cartPage.py

--- pages_/cartPage.py
+++ pages_/cartPage.py
@@ -1,17 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pages_.basePage import BasePage
 
-class CartPage():
+class CartPage(BasePage):
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
 
     def delete_firstProduct_from_cart(self):
-        firstProductDeleteButtonElement = self.driver.find_element(By.XPATH, "(//input[@value = 'Delete'])[1]")
-        firstProductDeleteButtonElement.click()
+       firstProductDeleteButtonElement = self._find_element(By.XPATH, "(//input[@value = 'Delete'])[1]")
+       self._click_to_element(firstProductDeleteButtonElement)
 
     def save_for_later_firstProduct_from_cart(self):
-        firstProductSaveForLeterButtonElement = self.driver.find_element(By.XPATH, "(//input[@name ='submit.save-for-later.8ac998b5-36b3-435b-8715-f53a826c6f1a'])[1]")
-        firstProductSaveForLeterButtonElement.click()
-
-
+        firstProductSaveForLeterButtonElement = self._find_element(By.XPATH, "(//input[@name ='submit.save-for-later.8ac998b5-36b3-435b-8715-f53a826c6f1a'])[1]")
+        self._click_to_element(firstProductSaveForLeterButtonElement)
 
