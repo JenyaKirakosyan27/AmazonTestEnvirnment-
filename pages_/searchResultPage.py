7050626741f51fd4cb030cdd51e785c5d49a3147
JenyaKirakosyan27 <147413285+JenyaKirakosyan27@users.noreply.github.com>
2023-10-25 23:18:43 +0400

    Update searchResultPage.py

--- pages_/searchResultPage.py
+++ pages_/searchResultPage.py
@@ -1,14 +1,15 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from pages_.basePage import BasePage
 
-class SearchResultPage():
+class SearchResultPage(BasePage):
     def __init__(self, driver: webdriver.Chrome):
         self.driver = driver
 
     def click_to_first_product(self):
-        firstProductElement = self.driver.find_element(By.XPATH, "(//div[@class='a-section aok-relative s-image-square-aspect'])[1]")
-        firstProductElement.click()
+        firstProductElement = self._find_element(By.XPATH, "(//div[@class='a-section aok-relative s-image-square-aspect'])[1]")
+        self._click_to_element(firstProductElement)
 
     def click_to_add_to_cart_button(self):
-        addToCartButtonElement = self.driver.find_element(By.XPATH, "//span[@id = 'submit.add-to-cart']")
-        addToCartButtonElement.click()
+        addToCartButtonElement = self._find_element(By.XPATH, "//span[@id = 'submit.add-to-cart']")
+        self._click_to_element(addToCartButtonElement)
