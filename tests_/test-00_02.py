87f360d644237d8b4ab36792678c415c58cf80b9
JenyaKirakosyan27 <147413285+JenyaKirakosyan27@users.noreply.github.com>
2023-10-26 11:08:29 +0400

    Update test-00_02.py

--- tests_/test-00_02.py
+++ tests_/test-00_02.py
@@ -6,8 +6,8 @@ from pages_.cartPage import CartPage
 driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.maximize_window()
-
 driver.get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fsign%2Fs%3Fk%3Dsign%2Bin%26ref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
+
 loginPageObj = LoginPage(driver)
 loginPageObj.fill_username_fild("jenyakirakosyan27@gmail.com")
 loginPageObj.click_to_continue_button()
