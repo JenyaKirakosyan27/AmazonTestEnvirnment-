3582d77145b7e659e7e8dee8620a68be2b9e42e0
JenyaKirakosyan27 <147413285+JenyaKirakosyan27@users.noreply.github.com>
2023-10-27 14:38:45 +0400

    Update searchResultPage.py

--- pages_/searchResultPage.py
+++ pages_/searchResultPage.py
@@ -10,6 +10,4 @@ class SearchResultPage(BasePage):
         firstProductElement = self._find_element(By.XPATH, "(//div[@class='a-section aok-relative s-image-square-aspect'])[1]")
         self._click_to_element(firstProductElement)
 
-    def click_to_add_to_cart_button(self):
-        addToCartButtonElement = self._find_element(By.XPATH, "//span[@id = 'submit.add-to-cart']")
-        self._click_to_element(addToCartButtonElement)
+ 
